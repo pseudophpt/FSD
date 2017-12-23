73ed4bfc56cd5f0f3facd338297408f3a3777a05
Pseudophpt <pseudophpt@gmail.com>
2017-12-22 23:59:37 -0500

    Add physical allocator functions

--- src/mem/phys_allocator.c
+++ src/mem/phys_allocator.c
@@ -3,7 +3,7 @@
 
 #include "phys_allocator.h"
 
-uint32_t *phys_page_stack = (uint32_t *)0xC07FFFFC;
+uint32_t *phys_page_stack = (uint32_t *)PHYS_STACK_BASE;
 
 void init_phys_page_stack (void) {
     for (int page = 0; page < (1 << 20); page ++) {
@@ -12,7 +12,28 @@ void init_phys_page_stack (void) {
             page = 0xC0800;
         
         /* Write page to stack and decrement stack pointer */
-        *phys_page_stack = (page << 12); 
         phys_page_stack --;
+        *phys_page_stack = (page << 12); 
     }   
+}
+
+uint32_t *pop_phys_page (void) {
+    uint32_t *page;
+    
+    /* If there are no pages left, fail */
+    if ((uint32_t)phys_page_stack >= (uint32_t)PHYS_STACK_BASE) {
+        while(1);
+    }
+    else {
+        page = (uint32_t *)(*phys_page_stack);
+        phys_page_stack ++;
+    }
+
+    return page;
+}
+
+void push_phys_page(uint32_t *page) {
+    /* Make space for the page and push it */
+    phys_page_stack --;
+    *phys_page_stack = (uint32_t)page;
 }
--- src/mem/phys_allocator.h
+++ src/mem/phys_allocator.h
@@ -2,8 +2,11 @@
 #define KERNEL_MEM_PHYS_ALLOCATOR_H
 
 #define PAGE_SIZE 0x1000
-#define PHYS_STACK_BASE (uint32_t *)0xC0400000;
+#define PHYS_STACK_BASE (uint32_t *)0xC0800000
+#define PHYS_STACK_TOP (uint32_t *)0xC0400000
 
+uint32_t *pop_phys_page (void); 
+void push_phys_page(uint32_t *page); 
 void init_phys_page_stack (void);
 
 #endif
